e5d3fc8e27da021596dae7431889a8692c9b68d9
sieunkim <sieunkr@gmail.com>
2020-06-20 15:48:59 +0900

    Query Server Getting Started

---
+++ query/src/test/java/com/example/query/QueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
